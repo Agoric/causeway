42399d903b9ab0fa4c4cefccad27aa61782dbf21
rabi-siddique <rabisiddique27@gmail.com>
2024-10-31 10:10:45 +0500

    feat: add route for uploading files

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import './lockdown.js';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const PORT = 8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 8080;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (_, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
