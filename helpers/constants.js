1d67a1e3e09f8cdacf31e2e94fd96791e5fa053e
rabi-siddique <rabisiddique27@gmail.com>
2025-06-12 17:44:43 +0500

    chore: update filters in constants.js

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,20 +1,20 @@
 export const networks = {
   mainnet: {
     container_name: 'log-slog',
-    cluster_name: 'puffynet',
+    cluster_name: 'instagoric',
     namespace_name: 'followmain',
     pod_name: 'follower-0',
   },
   emerynet: {
     container_name: 'log-slog',
-    cluster_name: 'emerynet',
-    namespace_name: 'instagoric',
+    cluster_name: 'instagoric',
+    namespace_name: 'emerynet',
     pod_name: 'validator-primary-0',
   },
   devnet: {
     container_name: 'log-slog',
-    cluster_name: 'devnet',
-    namespace_name: 'instagoric',
+    cluster_name: 'instagoric',
+    namespace_name: 'devnet',
     pod_name: 'validator-primary-0',
   },
 };
